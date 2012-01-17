aa267acf660cfff3003e2de4ad59a511cf713102
Neo Geek <neogeek0101@gmail.com>
2012-01-16 19:16:10 -0500

    Updated copyright year.

--- favicache.php
+++ favicache.php
@@ -10,7 +10,7 @@
  Author: Neo Geek
  Author URI: http://neo-geek.net/
  
- Copyright (c) 2011 Neo Geek
+ Copyright (c) 2012 Neo Geek
  Dual-licensed under both MIT and BSD licenses.
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
